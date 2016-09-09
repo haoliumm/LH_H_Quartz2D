78e13c957dbe30dfd0b589fa3d0c52698a871f05
liuhao <857035991@qq.com>
2016-09-09 16:38:30 +0800

    基本绘图实现 绘制 任意角星星

---
+++ LH-旋转的派大星/LH-旋转的派大星/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LH-旋转的派大星
+//
+//  Created by liuhao on 16/9/9.
+//  Copyright © 2016年 liuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LH-旋转的派大星/LH-旋转的派大星/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LH-旋转的派大星
+//
+//  Created by liuhao on 16/9/9.
+//  Copyright © 2016年 liuhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
